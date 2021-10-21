274edfa91687f95f9bc50feb838f11e7a6050b91
Randriamarosaina Sombiniaina Fitahiana <77749928+sombiniaina-fitahiana-randriamarosaina@users.noreply.github.com>
2021-10-21 12:19:00 +0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/madaroute';
+$config['base_url'] = 'https://madaroute.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
