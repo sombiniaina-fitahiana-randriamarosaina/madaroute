edb7087dff122d93651656cde3ccb6c628f0b686
Randriamarosaina Sombiniaina Fitahiana <77749928+sombiniaina-fitahiana-randriamarosaina@users.noreply.github.com>
2021-10-21 12:16:14 +0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -443,6 +443,7 @@ $config['csrf_protection'] = FALSE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['csrf_regenerate'] = TRUE;
 $config['csrf_exclude_uris'] = array();
 
